ebe161596997d5948f67b743a465f8d352fb4229
Ankit Singh <ankitsingh@ankits-air.lan>
2021-11-07 01:02:29 -0400

    flutter frontend

---
+++ globalfitness/android/app/src/main/kotlin/com/example/globalfitness/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.globalfitness
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ globalfitness/lib/screens/intro_screen.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GlobalFitness'),
+      ),
+      body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/cover.jpg'), fit: BoxFit.cover)),
+          child: Center(
+              child: Container(
+                  padding: EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.white70,
+                  ),
+                  child: Text(
+                    'Commit to be fit, dare to be great \n with Global Fitness',
+                    textAlign: TextAlign.center,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    style: TextStyle(fontSize: 20, shadows: [
+                      Shadow(
+                        offset: Offset(1.0, 1.0),
+                        blurRadius: 2.0,
+                        color: Colors.grey,
+                      )
+                    ]),
+                  )))),
+    );
+  }
+}
